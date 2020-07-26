3ffc94abd777f02b634623bb579d6cb0c6d8b877
budougumi0617 <budougumi0617@binc.jp>
2020-07-26 21:03:40 +0900

    :sparkles: start 36.valid-sudoku

---
+++ 36.valid-sudoku_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func Test_isValidSudoku(t *testing.T) {
+	type args struct {
+		board [][]byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValidSudoku(tt.args.board); got != tt.want {
+				t.Errorf("isValidSudoku() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
