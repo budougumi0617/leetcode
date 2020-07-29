830249a62589693f184454731fb8a2454b341c51
budougumi0617 <budougumi0617@binc.jp>
2020-07-29 22:11:16 +0900

    :sparkles: add skeleton

---
+++ 21.merge-two-sorted-lists.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=21 lang=golang
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (53.10%)
+ * Likes:    4441
+ * Dislikes: 603
+ * Total Accepted:    1M
+ * Total Submissions: 1.9M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * Merge two sorted linked lists and return it as a new sorted list. The new
+ * list should be made by splicing together the nodes of the first two lists.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+    
+}
+// @lc code=end
+
