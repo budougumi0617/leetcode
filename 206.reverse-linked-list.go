1589cc898d73aabf542932c879eb1a47c7e7e734
budougumi0617 <budougumi0617@binc.jp>
2020-07-30 18:47:17 +0900

    :sparkles: add skeleton for 206

---
+++ 206.reverse-linked-list.go
@@ -0,0 +1,46 @@
+package main
+
+/*
+ * @lc app=leetcode id=206 lang=golang
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (61.96%)
+ * Likes:    4685
+ * Dislikes: 91
+ * Total Accepted:    1M
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Reverse a singly linked list.
+ *
+ * Example:
+ *
+ *
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ *
+ *
+ * Follow up:
+ *
+ * A linked list can be reversed either iteratively or recursively. Could you
+ * implement both?
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+
+}
+
+// @lc code=end
---
+++ 206.reverse-linked-list_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Input: 1->2->3->4->5->NULL
+// Output: 5->4->3->2->1->NULL
+func Test_reverseList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "example",
+			args: args{
+				head: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val: 4,
+								Next: &ListNode{
+									Val: 5,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 5,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val: 2,
+							Next: &ListNode{
+								Val: 1,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
