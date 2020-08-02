fa2e6e7daa5c77572876919249089ec3aaf198e0
budougumi0617 <budougumi0617@binc.jp>
2020-08-02 21:09:16 +0900

    :sparkles: add skeleton

---
+++ 141.linked-list-cycle_test.go
@@ -0,0 +1,75 @@
+package main
+
+import "testing"
+
+func Test_hasCycle(t *testing.T) {
+	type args struct {
+		head *ListNode
+		pos  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "example",
+			args: args{
+				head: &ListNode{
+					Val: 3,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 0,
+							Next: &ListNode{
+								Val: -4,
+							},
+						},
+					},
+				},
+				pos: 1,
+			},
+			want: true,
+		},
+		{
+			name: "example3",
+			args: args{
+				head: &ListNode{
+					Val: 1,
+				},
+				pos: -1,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			l := findLast(tt.args.head)
+			p := findPos(tt.args.head, tt.args.pos)
+			l.Next = p
+			if got := hasCycle(tt.args.head); got != tt.want {
+				t.Errorf("hasCycle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func findLast(head *ListNode) *ListNode {
+	last := head
+	for last.Next != nil {
+		last = last.Next
+	}
+	return last
+}
+
+func findPos(head *ListNode, pos int) *ListNode {
+	if pos == -1 {
+		return nil
+	}
+	p := head
+	for i := 0; i < pos; i++ {
+		p = p.Next
+	}
+	return p
+}
