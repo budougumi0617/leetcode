ff922e89305ae08bc774afd1cbc161df06bde617
budougumi0617 <budougumi0617@binc.jp>
2020-08-05 21:55:29 +0900

    :white_check_mark: add test cases

--- 101.symmetric-tree_test.go
+++ 101.symmetric-tree_test.go
@@ -11,9 +11,46 @@ func Test_isSymmetric(t *testing.T) {
 		args args
 		want bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "example1",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Left: &TreeNode{
+						Val:   2,
+						Left:  &TreeNode{Val: 3},
+						Right: &TreeNode{Val: 4},
+					},
+					Right: &TreeNode{
+						Val:   2,
+						Left:  &TreeNode{Val: 4},
+						Right: &TreeNode{Val: 3},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "example2",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Left: &TreeNode{
+						Val:   2,
+						Right: &TreeNode{Val: 3},
+					},
+					Right: &TreeNode{
+						Val:   2,
+						Right: &TreeNode{Val: 3},
+					},
+				},
+			},
+			want: false,
+		},
 	}
+
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			if got := isSymmetric(tt.args.root); got != tt.want {
 				t.Errorf("isSymmetric() = %v, want %v", got, tt.want)
