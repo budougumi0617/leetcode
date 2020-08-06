c626faae46ed6c66e5205c21c2fc0f31a3450c9f
budougumi0617 <budougumi0617@binc.jp>
2020-08-06 22:14:49 +0900

    :sparkles: add skeleton

---
+++ 98.validate-binary-search-tree_test.go
@@ -0,0 +1,56 @@
+package main
+
+import "testing"
+
+func Test_isValidBST(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "example1",
+			args: args{
+				root: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val: 1,
+					},
+					Right: &TreeNode{
+						Val: 3,
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "example2",
+			args: args{
+				root: &TreeNode{
+					Val: 5,
+					Left: &TreeNode{
+						Val:   1,
+						Left:  &TreeNode{Val: 3},
+						Right: &TreeNode{Val: 6},
+					},
+					Right: &TreeNode{
+						Val: 4,
+					},
+				},
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValidBST(tt.args.root); got != tt.want {
+				t.Errorf("isValidBST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
