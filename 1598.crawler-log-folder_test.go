9e880a32b7737493375e05b4b5b324ec8dd7e5fc
budougumi0617 <budougumi0617@gmail.com>
2020-11-22 02:08:32 +0900

    :white_check_mark: 1598: add test

--- 1598.crawler-log-folder_test.go
+++ 1598.crawler-log-folder_test.go
@@ -3,19 +3,18 @@ package main
 import "testing"
 
 func Test_minOperations(t *testing.T) {
-	type args struct {
-		logs []string
-	}
 	tests := []struct {
 		name string
-		args args
+		logs []string
 		want int
 	}{
-		// TODO: Add test cases.
+		{name: "Example1", logs: []string{"d1/", "d2/", "../", "d21/", "./"}, want: 2},
+		{name: "Example2", logs: []string{"d1/", "d2/", "./", "d3/", "../", "d31/"}, want: 3},
+		{name: "Example3", logs: []string{"d1/", "../", "../", "../"}, want: 0},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := minOperations(tt.args.logs); got != tt.want {
+			if got := minOperations(tt.logs); got != tt.want {
 				t.Errorf("minOperations() = %v, want %v", got, tt.want)
 			}
 		})
