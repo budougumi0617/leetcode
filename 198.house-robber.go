b367ecc09bc37313fbace2111e13011a7c1a47b0
budougumi0617 <budougumi0617@gmail.com>
2020-11-28 00:04:01 +0900

    :sparkles: 198: add solution

--- 198.house-robber.go
+++ 198.house-robber.go
@@ -58,7 +58,23 @@ package main
 
 // @lc code=start
 func rob(nums []int) int {
-	return -1
+	if len(nums) == 0 {
+		return 0
+	}
+
+	dp := make([]int, len(nums)+1)
+	dp[1] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		dp[i+1] = max(dp[i], dp[i-1]+nums[i])
+	}
+	return dp[len(nums)]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 // @lc code=end
