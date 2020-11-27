139e4b6d2a67618074972b6c5f54313d004563a2
budougumi0617 <budougumi0617@gmail.com>
2020-11-27 11:37:27 +0900

    :sparkles: 198: add skeleton and test code

---
+++ 198.house-robber_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func Test_rob(t *testing.T) {
+	tests := [...]struct {
+		name string
+		nums []int
+		want int
+	}{
+		{name: "Example1", nums: []int{1, 2, 3, 1}, want: 4},
+		{name: "Example2", nums: []int{2, 7, 9, 3, 1}, want: 12},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rob(tt.nums); got != tt.want {
+				t.Errorf("rob() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
