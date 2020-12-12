1ca1936a0b1c72d999476da5e0421bed8722a296
budougumi0617 <budougumi0617@gmail.com>
2020-12-12 18:07:55 +0900

    :sparkles: add skeleton for 14

---
+++ 14.longest-common-prefix.go
@@ -0,0 +1,55 @@
+package main
+
+/*
+ * @lc app=leetcode id=14 lang=golang
+ *
+ * [14] Longest Common Prefix
+ *
+ * https://leetcode.com/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (35.80%)
+ * Likes:    3371
+ * Dislikes: 2044
+ * Total Accepted:    886.8K
+ * Total Submissions: 2.5M
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * Write a function to find the longest common prefix string amongst an array
+ * of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 0 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] consists of only lower-case English letters.
+ *
+ *
+ */
+
+// @lc code=start
+func longestCommonPrefix(strs []string) string {
+	return ""
+}
+
+// @lc code=end
---
+++ 14.longest-common-prefix_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func Test_longestCommonPrefix(t *testing.T) {
+	tests := [...]struct {
+		name string
+		strs []string
+		want string
+	}{
+		{name: "Example1", strs: []string{"flower", "flow", "flight"}, want: "fl"},
+		{name: "Example2", strs: []string{"dog", "racecar", "car"}, want: ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestCommonPrefix(tt.strs); got != tt.want {
+				t.Errorf("longestCommonPrefix() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
